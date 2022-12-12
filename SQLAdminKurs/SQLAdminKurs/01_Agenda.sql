25f9536c257ef06a906509af4fac52a4d79621bb
Rauchendraser <andreasr@ppedv.de>
2022-12-12 12:36:49 +0100

    SQL Setup VM und DB

---
+++ SQLAdminKurs/SQLAdminKurs/01_Agenda.sql
@@ -0,0 +1,22 @@
+--Wartung Optimierung Defragmentierung , Cleanup		 2014 Alexander--> SQL2019 /2022
+
+--
+
+/*
+
+Setup.. seit SQL 2016 rel viel zum Thema bet practice
+Serversettings
+DB Settings  DBObjekte
+Agent Jobs-- Schritte, Zeitpläne, Warnungen --> Email senden
+Backup Restore
+Security
+Monitoring / Tuning
+
+Indizies
+
+
+
+*/
+
+
+
